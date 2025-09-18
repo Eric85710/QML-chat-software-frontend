3bcee5c411ba79e4fe624397bd7951c720a7dd30
Eric Chen <ericchen87887@gmail.com>
2025-09-18 20:31:32 +0800

    add eventbus header

--- Server_Page_status/eventbus.h
+++ Server_Page_status/eventbus.h
@@ -1,15 +1,28 @@
+// EventBus.h
 #ifndef EVENTBUS_H
 #define EVENTBUS_H
 
 #include <QObject>
+#include <QString>
 
-class EventBus : public QObject
-{
+class EventBus : public QObject {
     Q_OBJECT
 public:
-    explicit EventBus(QObject *parent = nullptr);
+    explicit EventBus(QObject *parent = nullptr) : QObject(parent) {}
+
+    // 單例存取
+    static EventBus* instance() {
+        static EventBus bus;
+        return &bus;
+    }
 
 signals:
+    void sendMessage(const QString &msg);  // 廣播訊息
+
+public slots:
+    void postMessage(const QString &msg) { // 發送事件
+        emit sendMessage(msg);
+    }
 };
 
 #endif // EVENTBUS_H
